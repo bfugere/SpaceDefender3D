45dc1612fdcb524a0bee1aa6d31709d3dd5e8877
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-10-29 15:38:49 -0400

    Reload on Collisions

--- SpaceDefender3D/Assets/Scripts/CollisionHandler.cs
+++ SpaceDefender3D/Assets/Scripts/CollisionHandler.cs
@@ -1,11 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float waitTime = 1f;
+
+    PlayerControls playerControlsScript;
+
+    void Start()
+    {
+        playerControlsScript = GetComponent<PlayerControls>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{name} was triggered by: {other.gameObject.name}");
+        StartCoroutine(StartCrashSequence(waitTime));
+    }
+
+    IEnumerator StartCrashSequence(float waitTime)
+    {
+        playerControlsScript.enabled = false;
+
+        yield return new WaitForSeconds(waitTime);
+
+        playerControlsScript.enabled = true;
+        ReloadLevel();
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
