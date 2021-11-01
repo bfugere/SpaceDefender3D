0559e2f9d15658ff8cea01ec57134bf9c74cf751
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-01 11:15:57 -0400

    Initial Scoreboard Setup

--- SpaceDefender3D/Assets/Scripts/Enemy.cs
+++ SpaceDefender3D/Assets/Scripts/Enemy.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        DestroyEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void DestroyEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
