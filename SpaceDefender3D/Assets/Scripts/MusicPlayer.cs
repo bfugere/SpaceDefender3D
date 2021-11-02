259d767464b035ec59ef7108cd7985c693e15f0c
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-02 12:20:07 -0400

    Music & SFX

---
+++ SpaceDefender3D/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int numberOfMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+
+        if (numberOfMusicPlayers > 1)
+            Destroy(gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+    }
+}
